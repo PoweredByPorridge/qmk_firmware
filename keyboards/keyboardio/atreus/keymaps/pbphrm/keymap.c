3f7522a58c5b4ee0bfe94c9d1fa121c2b3fd8e79
Duncan Tweed <duncan.tweed@gmail.com>
2021-01-31 10:21:30 +0000

    Put bas the mods on the bottom row, removed the ALT hrms, and added some tap dances for quotes and hashes.

--- keyboards/keyboardio/atreus/keymaps/pbphrm/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/pbphrm/keymap.c
@@ -1,22 +1,15 @@
 #include QMK_KEYBOARD_H
 
- // THIS FILE NEEDS WORK!
- // - [X] Arrows to be removed from old place 
- // - [X] add Any() code to configurator
- // - [X] Enter added to layer 1
- // - [X] move stuff from old brax position
-
-
 // Left-hand home row mods
 // #define GUI_A LGUI_T(KC_A)
-#define ALT_S LALT_T(KC_S)
+// #define ALT_S LALT_T(KC_S)
 #define SFT_D LSFT_T(KC_D)
 #define CTL_F LCTL_T(KC_F)
 
 // Right-hand home row mods
 #define CTL_J RCTL_T(KC_J)
 #define SFT_K RSFT_T(KC_K)
-#define ALT_L LALT_T(KC_L)
+// #define ALT_L LALT_T(KC_L)
 // #define GUI_SCLN RGUI_T(KC_SCLN)
 
 
@@ -24,7 +17,9 @@
 // Tap Dance keycodes
 enum td_keycodes {
     L_BRAX, // Used in the keymap, add additional keycodes for each tapdance.
-    R_BRAX
+    R_BRAX,
+    QUOTS,  
+    TACHE   
 };
 
 // Define a type containing as many tapdance states as you need
@@ -49,13 +44,18 @@ void lbrax_reset(qk_tap_dance_state_t *state, void *user_data);
 void rbrax_finished(qk_tap_dance_state_t *state, void *user_data);
 void rbrax_reset(qk_tap_dance_state_t *state, void *user_data);
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_A, ALT_S, SFT_D, CTL_F, KC_G, KC_H, CTL_J, SFT_K, ALT_L, KC_SCLN, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NUBS, KC_SLSH, KC_N, KC_M, KC_COMM, KC_DOT, KC_ENT, KC_NO, KC_NO, KC_NO, LCTL_T(KC_ESC), MO(1), KC_LGUI, KC_SPC, MO(2), LALT_T(KC_TAB), KC_NO, KC_NO, KC_NO),
-	[1] = LAYOUT(KC_EXLM, KC_AT, LSFT(KC_3), KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_DEL, KC_BSPC, KC_F24, LALT(KC_F3), KC_NO, KC_NO, TD(L_BRAX), TD(R_BRAX), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LCTL(KC_F12), LALT(KC_X), KC_NO, LSFT(KC_NUHS), KC_NUHS, KC_NUBS, KC_SLSH, KC_MINS, KC_EQL, KC_QUOT, KC_AT, KC_DQUO, KC_NO, KC_NO, KC_NO, MO(3), KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO),
-	[2] = LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_F24, LALT(KC_F3), KC_NO, KC_NO, TD(L_BRAX), TD(R_BRAX), KC_HOME, KC_PGDN, KC_PGUP, KC_END, LCTL(KC_F12), LALT(KC_X), KC_NO, KC_NO, KC_NO, KC_NUBS, KC_SLSH, KC_MINS, KC_EQL, KC_COMM, KC_DOT, KC_DQUO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SPC, KC_SPC, KC_NO, MO(3), KC_NO, KC_NO, KC_NO),
-	[3] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_ASTG, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL, LALT(KC_DEL), KC_NO, KC_NO, KC_VOLD, KC_MPLY, KC_MPLY, KC_MPRV, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
-};
+void quots_finished(qk_tap_dance_state_t *state, void *user_data);
+void quots_reset(qk_tap_dance_state_t *state, void *user_data);
 
+void tache_finished(qk_tap_dance_state_t *state, void *user_data);
+void tache_reset(qk_tap_dance_state_t *state, void *user_data);
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_A, KC_S, LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, RCTL_T(KC_J), RSFT_T(KC_K), KC_L, KC_SCLN, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NUBS, KC_SLSH, KC_N, KC_M, KC_COMM, KC_DOT, KC_ENT, LCTL_T(KC_ESC), KC_LALT, KC_LSFT, KC_LGUI, MO(1), KC_SPC, KC_SPC, MO(2), TD(TACHE), TD(QUOTS), KC_LCTL, LALT_T(KC_TAB)),
+    [1] = LAYOUT(KC_EXLM, KC_AT, LSFT(KC_3), KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_DEL, KC_BSPC, KC_F24, LALT(KC_F3), KC_NO, KC_NO, TD(L_BRAX), TD(R_BRAX), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LCTL(KC_F12), LALT(KC_X), KC_NO, KC_NO, KC_NO, KC_NUBS, KC_SLSH, KC_MINS, KC_EQL, TD(QUOTS), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MO(3), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO),
+    [2] = LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_F24, LALT(KC_F3), KC_NO, KC_NO, TD(L_BRAX), TD(R_BRAX), KC_HOME, KC_PGDN, KC_PGUP, KC_END, LCTL(KC_F12), LALT(KC_X), KC_NO, KC_NO, KC_NO, KC_NUBS, KC_SLSH, KC_MINS, KC_EQL, KC_COMM, KC_DOT, KC_DQUO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MO(3), KC_NO, KC_NO, KC_NO),
+    [3] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_ASTG, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL, LALT(KC_DEL), KC_NO, KC_NO, KC_VOLD, KC_MPLY, KC_MPLY, KC_MPRV, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+};
 
 /*
 // tapping term per key
@@ -155,11 +155,76 @@ void rbrax_reset(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
+void quots_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case SINGLE_TAP:
+            // register_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            register_code16(KC_QUOT);
+            break;
+        case DOUBLE_TAP:
+            register_code16(KC_AT);
+            break;
+        case TRIPLE_TAP: // Allow nesting of 2 parens `((` within tapping term
+            // register_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            register_code16(KC_DQUO);
+    }
+}
+
+void quots_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_TAP:
+            unregister_code16(KC_QUOT);
+            // unregister_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            break;
+        case DOUBLE_TAP:
+            unregister_code16(KC_AT);
+            break;
+        case TRIPLE_TAP:
+            unregister_code16(KC_DQUO);
+            // unregister_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+    }
+}
+
+void tache_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case SINGLE_TAP:
+            // register_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            register_code16(KC_NUHS);
+            break;
+        case DOUBLE_TAP:
+            register_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here        
+            register_code16(KC_NUHS);
+            break;
+        case TRIPLE_TAP: // Allow nesting of 2 parens `((` within tapping term
+            // register_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            register_code16(KC_GRV);
+    }
+}
+
+void tache_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_TAP:
+            unregister_code16(KC_NUHS);
+            // unregister_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            break;
+        case DOUBLE_TAP:
+            unregister_code16(KC_NUHS);
+            unregister_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            break;
+        case TRIPLE_TAP:
+            unregister_code16(KC_GRV);
+            // unregister_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+    }
+}
 
 
 // Define `ACTION_TAP_DANCE_FN_ADVANCED()` for each tapdance keycode, passing in `finished` and `reset` functions
 qk_tap_dance_action_t tap_dance_actions[] = {
     [L_BRAX] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lbrax_finished, lbrax_reset),
-    [R_BRAX] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rbrax_finished, rbrax_reset)
+    [R_BRAX] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rbrax_finished, rbrax_reset),
+    [QUOTS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, quots_finished, quots_reset),
+    [TACHE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tache_finished, tache_reset)
 };
 
